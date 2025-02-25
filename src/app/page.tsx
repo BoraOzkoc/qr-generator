91906436aded6657ab5fad590276079fa68a3646
Bora Özkoç <boraozkoc@hotmail.com>
2025-02-25 13:08:46 +0300

    Remove commented-out code from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import WelcomePage from "./components/WelcomePage";
 export default function Home() {
   return (
     <div className=" container mx-auto w-full h-screen flex justify-center items-center">
-      {/*       <Test text="https://boraozkoc.com" />
-       */}
       <WelcomePage />
     </div>
   );
