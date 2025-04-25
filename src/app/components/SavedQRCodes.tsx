9e5b1e3a0d840239fc0cb49e9abcd0206d280402
Bora Özkoç <boraozkoc@hotmail.com>
2025-04-25 21:12:52 +0200

    Update SavedQRCodes component to improve empty state message and adjust background color for better visibility.

--- src/app/components/SavedQRCodes.tsx
+++ src/app/components/SavedQRCodes.tsx
@@ -35,13 +35,13 @@ function SavedQRCodes() {
           <p className="text-2xl font-bold py-6">Created QR Codes</p>
         </LinearGradient>
         {savedData.length === 0 ? (
-          <p className="text-xl">No created QR Codes detected</p>
+          <p className="text-xl">Empty</p>
         ) : (
           <div className="flex flex-col justify-center items-center gap-4">
             {savedData.map((item, index) => (
               <div
                 key={index}
-                className="flex flex-col rounded-lg border-2 bg-[#bb6eff] w-fit justify-center text-center p-3 items-center"
+                className="flex flex-col rounded-lg border-2 bg-[#8D62F6] w-fit justify-center text-center p-3 items-center"
               >
                 <span className="flex justify-center text-center items-center">
                   <QRCodePreview url={item} />
